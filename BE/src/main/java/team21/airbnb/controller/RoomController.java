de0ced657002320ee545366a82c2f2279d1b2eb4
dukcode <duk9741@gmail.com>
2022-05-26 18:05:17 +0900

    refactor: getRoomChargeDistribution() url 수정

--- BE/src/main/java/team21/airbnb/controller/RoomController.java
+++ BE/src/main/java/team21/airbnb/controller/RoomController.java
@@ -2,17 +2,19 @@ package team21.airbnb.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team21.airbnb.dto.response.RoomChargeDistributionResponse;
 import team21.airbnb.service.RoomService;
 
 @RestController
+@RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
     private final RoomService roomService;
 
-    @GetMapping("/room_charge_distribution")
+    @GetMapping("/charge_distribution")
     public RoomChargeDistributionResponse getRoomChargeDistribution() {
         return roomService.getRoomChargeDistribution();
     }
