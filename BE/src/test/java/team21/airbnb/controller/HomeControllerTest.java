ad7749da20ee34f985c4bc7fa6ada4d761ee1738
dukcode <duk9741@gmail.com>
2022-05-26 16:18:07 +0900

    test: HomeControllerTest WebMvcTest로 변경

--- BE/src/test/java/team21/airbnb/controller/HomeControllerTest.java
+++ BE/src/test/java/team21/airbnb/controller/HomeControllerTest.java
@@ -1,59 +1,56 @@
 package team21.airbnb.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 import team21.airbnb.domain.Room;
 import team21.airbnb.dto.response.RoomChargeDistributionResponse;
-import team21.airbnb.repository.RoomRepository;
 import team21.airbnb.service.RoomService;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
-
-    @LocalServerPort int port;
-
-    @Autowired TestRestTemplate restTemplate;
 
     public static final int MIN_ROOM_CHARGE = 10005;
     public static final int MAX_ROOM_CHARGE = MIN_ROOM_CHARGE + 1000;
 
-
-    @Autowired RoomRepository roomRepository;
-    @Autowired RoomService roomService;
-    @Autowired HomeController homeController;
+    @Autowired MockMvc mockMvc;
+    @MockBean RoomService roomService;
 
     @Test
     @DisplayName("가격 분포 요청 시 응답한다")
-    public void getChargeDistribution() {
+    public void getChargeDistribution() throws Exception {
         // given
         Room room1 = Room.builder().name("room1").roomCharge(MIN_ROOM_CHARGE).build();
         Room room2 = Room.builder().name("room2").roomCharge(MIN_ROOM_CHARGE).build();
         Room room3 = Room.builder().name("room3").roomCharge(MAX_ROOM_CHARGE).build();
 
-        roomService.save(room1);
-        roomService.save(room2);
-        roomService.save(room3);
+        HashMap<Integer, Integer> graph = new HashMap<>();
+        graph.put(10010, 2);
+        graph.put(11010, 1);
+        given(roomService.getRoomChargeDistribution())
+                .willReturn(new RoomChargeDistributionResponse(MIN_ROOM_CHARGE, MAX_ROOM_CHARGE,
+                        graph));
 
-        String url = "http://localhost:" + port + "/room_charge_distribution";
+        // when & then
+        mockMvc.perform(get("/room_charge_distribution"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.minRoomCharge").value(MIN_ROOM_CHARGE))
+                .andExpect(jsonPath("$.maxRoomCharge").value(MAX_ROOM_CHARGE))
+                .andExpect(jsonPath("$.graph['10010']").value(2))
+                .andExpect(jsonPath("$.graph['11010']").value(1))
+                .andDo(print());
 
-        // when
-        RoomChargeDistributionResponse roomChargeDistribution
-                = restTemplate.getForObject(url, RoomChargeDistributionResponse.class);
 
-        // then
-        assertThat(roomChargeDistribution.getMinRoomCharge()).isEqualTo(MIN_ROOM_CHARGE);
-        assertThat(roomChargeDistribution.getMaxRoomCharge()).isEqualTo(MAX_ROOM_CHARGE);
-        assertThat(roomChargeDistribution.getGraph()
-                .get(room1.getDistributedRoomCharge(RoomService.DISTRIBUTION_RANGE))).isEqualTo(2);
-        assertThat(roomChargeDistribution.getGraph()
-                .get(room3.getDistributedRoomCharge(RoomService.DISTRIBUTION_RANGE))).isEqualTo(1);
     }
 
 }
