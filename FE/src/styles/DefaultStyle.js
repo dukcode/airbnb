264d4f97fea9f14f72f4d610f4dcc01953bf2fa0
Hemudi <ksum1205@naver.com>
2022-05-27 11:57:54 +0900

    Design: GlobalStyle, Default Theme 적용
    Co-authored-by: jimmy neutron <dlwnstjrzz@users.noreply.github.com>

---
+++ FE/src/styles/DefaultStyle.js
@@ -0,0 +1,33 @@
+const fontSize = {
+  xxLarge: '34px',
+  xLarge: '24px',
+  large: '20px',
+  medium: '16px',
+  small: '14px',
+  xSmall: '12px',
+};
+
+const fontWeight = {
+  bold: 700,
+  medium: 500,
+  normal: 400,
+};
+
+const colors = {
+  white: '#FFFFFF',
+  grey6: '#F5F5F7',
+  grey5: '#E0E0E0',
+  grey4: '#BDBDBD',
+  grey3: '#828282',
+  grey2: '#4F4F4F',
+  grey1: '#333333',
+  green: '#118917',
+  pink: '#E84C60',
+  black: '#010101',
+};
+
+export const defaultTheme = {
+  fontSize,
+  fontWeight,
+  colors,
+};
