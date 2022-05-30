0715ecf229efdc87ced230fdca1937066d1fc2c8
Hemudi <ksum1205@naver.com>
2022-05-30 17:30:49 +0900

    refactor: menuData 상수로 분리
    Co-authored-by: jimmy neutron <dlwnstjrzz@users.noreply.github.com>

--- FE/src/components/header/gnb/menuBar/index.tsx
+++ FE/src/components/header/gnb/menuBar/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
+
 import * as Styled from 'components/header/gnb/menuBar/menubar.style';
+import menuData from 'constants/menuData';
 
 function MenuBar() {
-  type MenuType = {
-    id: number;
-    title: string;
-  };
-
-  const menuData: MenuType[] = [
-    { id: 1, title: '숙소' },
-    { id: 2, title: '체험' },
-    { id: 3, title: '온라인 체험' },
-  ];
-
   return (
     <Styled.MenuList>
       {menuData.map(({ id, title }) => (
---
+++ FE/src/constants/menuData.ts
@@ -0,0 +1,12 @@
+type MenuType = {
+  id: number;
+  title: string;
+};
+
+const menuData: MenuType[] = [
+  { id: 1, title: '숙소' },
+  { id: 2, title: '체험' },
+  { id: 3, title: '온라인 체험' },
+];
+
+export default menuData;
