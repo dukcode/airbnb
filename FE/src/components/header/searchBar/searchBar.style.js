a3deb6b0b94a613584447c8fb6bbb28b2f939ec3
Hemudi <ksum1205@naver.com>
2022-05-30 17:33:17 +0900

    design: searchBarItem 디자인 변경
    Co-authored-by: jimmy neutron <dlwnstjrzz@users.noreply.github.com>

--- FE/src/components/header/searchBar/searchBar.style.js
+++ FE/src/components/header/searchBar/searchBar.style.js
@@ -6,10 +6,16 @@ const SearchBarWrapper = styled.div`
   align-items: center;
   width: 916px;
   height: 76px;
-  border: 1px solid ${({ theme: { colors } }) => colors.black};
+  border: 1px solid ${({ theme: { colors } }) => colors.grey4};
   border-radius: 60px;
   background: ${({ theme: { colors } }) => colors.white};
   padding: 18px;
 `;
 
-export { SearchBarWrapper };
+const ItemWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  padding-left: 25px;
+`;
+
+export { SearchBarWrapper, ItemWrapper };
