e49a6041b46ec4b926cf25aa4583c9449ed70b1a
Hemudi <ksum1205@naver.com>
2022-05-30 17:25:16 +0900

    feat: Icon 컴포넌트에 DefaultIconProps 추가
    Co-authored-by: jimmy neutron <dlwnstjrzz@users.noreply.github.com>

--- FE/src/components/Icons/MinusIcon.tsx
+++ FE/src/components/Icons/MinusIcon.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-interface IconProps {
-  color: string;
-}
+import DefaultIconProps from 'components/Icons/DefaultIconProps';
 
-function MinusIcon({ color }: IconProps) {
+function MinusIcon({ color, size = { width: '36', height: '36' } }: DefaultIconProps) {
   return (
-    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={size.width} height={size.height} viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M18 33C26.2843 33 33 26.2843 33 18C33 9.71573 26.2843 3 18 3C9.71573 3 3 9.71573 3 18C3 26.2843 9.71573 33 18 33Z"
         stroke={color}
