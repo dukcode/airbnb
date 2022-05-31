6b42f4aa6fec1388e1bfe1192cb01444c4e06327
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-05-31 12:16:37 +0900

    feat/버튼별로 해당하는 모달이 팝업되게 상태 설정

--- FE/src/components/header/searchBar/index.tsx
+++ FE/src/components/header/searchBar/index.tsx
@@ -1,17 +1,21 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
 import * as Styled from 'components/header/searchBar/searchBar.style';
 import Line from 'components/Icons/Line';
 import SearchBarItem from 'components/header/searchBar/searchBarItem';
 import SearchIcon from 'components/Icons/SearchIcon';
 import { ActionType, reducer } from 'components/header/searchBar/contentReducer';
+import PriceModal from './priceModal';
+import { customStyles } from './priceModal/PriceModalInfo.style';
 
 const initState = { checkIn: '날짜 입력', checkOut: '날짜 입력', price: '금액대 설정', guest: '게스트 추가' };
 
 function SearchBar() {
   const [state, dispatch] = useReducer(reducer, initState);
+  const [clickTitle, setClickTitle] = useState('');
 
   const clickItem = (keyData, valueData) => () => {
+    setClickTitle(keyData);
     dispatch({
       type: ActionType.SET_CONTENTS,
       // key -> initState key
@@ -30,6 +34,14 @@ function SearchBar() {
       <Styled.ItemWrapper>
         <SearchBarItem title="요금" contents={state.price} onClick={clickItem('price', '100,000 ~ 1,000,000')} />
       </Styled.ItemWrapper>
+      <PriceModal
+        style={customStyles}
+        isClick={clickTitle}
+        lowPrice={10000}
+        highPrice={500000}
+        averagePrice={2000}
+        yCoordinate={[2, 4]}
+      />
       <Line />
       <Styled.ItemWrapper>
         <SearchBarItem title="인원" contents={state.guest} onClick={clickItem('guest', '게스트 3명, 유아 2명')} />
