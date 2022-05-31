592aa66ae99b3bd588a009c944b54e882e8bad89
dukcode <duk9741@gmail.com>
2022-05-31 16:43:32 +0900

    feat: 가격 분포 api 동작 방식 변경
    - 가격 리스트만 반환하도록 변경
    - request DTO 삭제
    - RequestParam으로 동작하도록 변경
    - Repository에서 가격을 가져올 때 오름차순 정렬
    - response DTO 삭제 및 List<Integer> 직접 반환

--- BE/src/main/java/team21/airbnb/controller/RoomController.java
+++ BE/src/main/java/team21/airbnb/controller/RoomController.java
@@ -1,24 +1,22 @@
 package team21.airbnb.controller;
 
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import team21.airbnb.dto.request.RoomChargeDistributionRequest;
-import team21.airbnb.dto.response.RoomChargeDistributionResponse;
 import team21.airbnb.service.RoomService;
 
 @RestController
-@RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
     private final RoomService roomService;
 
-    @GetMapping("/charge_distribution")
-    public RoomChargeDistributionResponse getRoomChargeDistribution(
-            @RequestBody RoomChargeDistributionRequest roomChargeDistributionRequest) {
-        return roomService.getRoomChargeDistribution(roomChargeDistributionRequest);
+    @GetMapping("/rooms/prices")
+    public List<Integer> getAvailableRoomCharges(@RequestParam LocalDate checkInDate,
+            @RequestParam LocalDate checkoutDate) {
+        return roomService.getAvailableRoomCharges(checkInDate, checkoutDate);
     }
 }
