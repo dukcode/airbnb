c5c37b4136caa1a2db20f22b51cda5f54e78bea3
Hemudi <ksum1205@naver.com>
2022-05-31 20:02:06 +0900

    feat: Calendar 모듈 Text 컴포넌트 구현

---
+++ FE/src/components/calender/components/Text/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as Styled from 'components/calender/components/Text/text.style';
+
+export interface TextProps {
+  fontSize?: number;
+  fontUnit?: string;
+  fontColor?: string;
+  fontWeight?: string;
+  children?: React.ReactNode;
+}
+
+const Text = ({ children, ...props }: TextProps) => {
+  return <Styled.Text {...props}>{children}</Styled.Text>;
+};
+
+Text.defaultProps = {
+  fontSize: 12,
+  fontUnit: 'px',
+  fontColor: '#000',
+  fontWeight: 'normal',
+  children: 'test',
+};
+
+export default Text;
