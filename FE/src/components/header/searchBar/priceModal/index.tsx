4059d09d44664549e31cb22b2eed357a93e223ad
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-05-31 20:29:38 +0900

    feat/요금 모달창에 슬라이드 구현

--- FE/src/components/header/searchBar/priceModal/index.tsx
+++ FE/src/components/header/searchBar/priceModal/index.tsx
@@ -5,16 +5,58 @@ import { moneyToWon } from 'utils/utils';
 import * as Styled from 'components/header/searchBar/priceModal/PriceModalInfo.style';
 
 interface ModalProps {
-  lowPrice: number;
-  highPrice: number;
-  averagePrice: number;
-  yCoordinate: number[];
+  priceData: number[];
   isClick: string;
   style: any;
 }
 
-function PriceModal({ style, isClick, lowPrice, highPrice, averagePrice, yCoordinate }: ModalProps) {
+interface Coordinate {
+  xRange: number;
+  yCoordinate: number[];
+}
+
+function PriceModal({ style, isClick, priceData }: ModalProps) {
+  const lowPrice = priceData[0];
+  const highPrice = priceData[priceData.length - 1];
+  const averagePrice = moneyToWon(
+    priceData.reduce((acc: number, el: number) => {
+      return acc + el;
+    }, 0) / priceData.length,
+  );
   const isOpen: boolean = isClick === 'price';
+
+  function makeYCoordinate() {
+    const singleX: number = (highPrice - lowPrice) / 50;
+    const yArr: number[] = [];
+    const countArr: number[] = [];
+    let count = 0;
+    let singlePrice = lowPrice + singleX;
+
+    priceData.forEach((price) => {
+      if (price <= singlePrice) {
+        count += 1;
+      } else {
+        singlePrice += singleX;
+        countArr.push(count);
+        count = 0;
+        count += 1;
+      }
+    });
+
+    const highCount = Math.max(...countArr);
+
+    countArr.forEach((count) => {
+      const priceRatio: number = 114 / highCount;
+      yArr.push(priceRatio * count);
+    });
+    return yArr;
+  }
+
+  const coordinateData: Coordinate = {
+    xRange: 7,
+    yCoordinate: makeYCoordinate(),
+  };
+
   return (
     <Modal isOpen={isOpen} style={style}>
       <Styled.ModalInfo>
@@ -24,9 +66,8 @@ function PriceModal({ style, isClick, lowPrice, highPrice, averagePrice, yCoordi
           <Styled.RowLine>-</Styled.RowLine>
           <Styled.Price>{moneyToWon(highPrice)}+</Styled.Price>
         </Styled.PriceWrapper>
-        <Styled.Average>평균 1박 요금은 {moneyToWon(averagePrice)} 입니다. </Styled.Average>
-        <Styled.Graphs />
-        {/* <Styled.Graph yCoordinate={yCoordinate} /> */}
+        <Styled.Average>평균 1박 요금은 {averagePrice} 입니다. </Styled.Average>
+        <Styled.Graphs coordinate={coordinateData} />
       </Styled.ModalInfo>
     </Modal>
   );
