e761e347d026cb4276d01c8936e7c86a4935e461
Jimmy <qny123@naver.com>
2022-06-02 16:39:18 +0900

    chore/모달 props type model파일로 분류

---
+++ FE/src/model/priceModal.model.ts
@@ -0,0 +1,11 @@
+interface ModalProps {
+  isClick: string;
+  style: unknown;
+}
+
+interface Coordinate {
+  xRange: number;
+  yCoordinate: number[];
+}
+
+export type { ModalProps, Coordinate };
