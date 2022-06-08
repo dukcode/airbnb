f220021c0df9f78026195660cdbd5ce4e7dc23df
Hemudi <ksum1205@naver.com>
2022-06-08 22:26:16 +0900

    fix: context 위치 수정

--- FE/src/components/header/index.tsx
+++ FE/src/components/header/index.tsx
@@ -3,22 +3,14 @@ import React from 'react';
 import GNB from 'components/header/gnb';
 import * as Styled from 'components/header/header.style';
 
-import { ModalContext } from 'components/context/ModalContext';
-import { GuestModalContext } from 'components/context/GuestModalContext';
-import { composeProvider } from 'utils/utils';
 import SearchBar from './searchBar';
 
 function Header() {
-  const providers = [ModalContext, GuestModalContext];
-  const ContextProvider = composeProvider(providers);
-
   return (
     <Styled.Header>
       <Styled.HeaderWrapper>
         <GNB />
-        <ContextProvider>
-          <SearchBar isSmallSize={false} />
-        </ContextProvider>
+        <SearchBar isSmallSize={false} />
       </Styled.HeaderWrapper>
     </Styled.Header>
   );
